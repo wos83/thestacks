09cfb34fb7cb9e1c652bc61214b123ce96a23fea
Willian Santos <m16@m16.com.br>
2023-03-11 16:07:37 -0300

    Initial Commit

---
+++ TheStacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheStacks
+//
+//  Created by Willian Santos on 3/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheStacks/TheStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheStacksApp.swift
+//  TheStacks
+//
+//  Created by Willian Santos on 3/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TheStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
